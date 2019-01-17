7e1eb5c52a8f4ba3b23f7075ea578b954d471ac7
rhagen <rhagen@pivotal.io>
2019-01-17 08:10:46 -0800

    adding config.js

---
+++ src/config.js
@@ -0,0 +1,30 @@
+
+
+const config = {
+    gemfire: {
+        region: process.env.REGION || "default",
+        type: process.env.TYPE || "PROXY"
+    },
+    /**
+     * Supports setting SESSION_STORE variable as env variable or in Credhub
+     * @returns {*}
+     */
+    getSessionCachingStrategy() {
+       return config.getEnvironmentVariable("SESSION_STORE", "local");
+    },
+    getEnvironmentVariable(name, defaultValue){
+        let vcap = JSON.parse(process.env.VCAP_SERVICES);
+        if (vcap[name] !== undefined) {
+            return vcap[name];
+        }
+
+        if (vcap["credhub"] !== undefined) {
+            return vcap["credhub"][0].credentials[name];
+        }
+
+        return defaultValue;
+    }
+
+};
+
+export default config;
